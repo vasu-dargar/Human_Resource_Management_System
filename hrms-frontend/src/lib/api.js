7135552cb6a3eb3426bb89881a299b98795930c8
Vasu Dargar <vasudargar@gmail.com>
2026-02-11 05:38:44 +0530

    Final commit

--- hrms-frontend/src/lib/api.js
+++ hrms-frontend/src/lib/api.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+console.log("API BASE URL:", import.meta.env.VITE_API_BASE_URL);
+
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
 });
